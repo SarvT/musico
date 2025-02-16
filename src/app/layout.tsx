97fdce1224400d49b31d4434436450546cacfbd0
SarvT <91484197+SarvT@users.noreply.github.com>
2025-02-16 21:30:04 +0530

    branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,20 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'musico',
   description: 'A music player which gives a soothing experience(as a wonderland)',
+  icons:{
+    icon:[
+      {
+        media:"(prefers-color-scheme: light)",
+        url:"/images/icon.svg",
+        href:"/images/icon.svg",
+      },
+      {
+        media:"(prefers-color-scheme: dark)",
+        url:"/images/icon.svg",
+        href:"/images/icon.svg",
+      },
+    ]
+  }
 }
 
 export default function RootLayout({
